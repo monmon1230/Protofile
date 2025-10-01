cfa7b17d5c00e58af9a76b1b45ac8d9ab001e3a9
mina magdy <mina@magdy.io>
2025-10-01 19:38:31 +0300

    fix: add index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import HomePage from './Components/HomePage/Pages/HomePage'
-import { Route, Routes, HashRouter } from 'react-router'
+import { Route, BrowserRouter, Routes, HashRouter } from 'react-router'
 import { Container } from 'react-bootstrap'
 import AboutPage from './Components/HomePage/Pages/AboutPage'
 import Contactme from './Components/HomePage/Pages/Contactme'
@@ -17,19 +17,17 @@ function App() {
   return (
    <div className=' w-screen h-screen  p-2  bg-gradient-to-br from-blue-900 via-black to-red-900'>
   
-<HashRouter>
- <NavBar/>
- <div className={`mt-[${Dimentions.navHeight}]`}>
-<Routes>
-    <Route path='/' element={<HomePage/>} />
-       <Route path='/about' element={<AboutPage/>} />
-          <Route path='/contact' element={<Contactme/>} />
-             <Route path='/projects' element={<Projects/>} />
-  </Routes>
-
- </div>
-  
-</HashRouter>
+<BrowserRouter basename="/Protofile">
+  <NavBar />
+  <div className={`mt-[${Dimentions.navHeight}]`}>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<Contactme />} />
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
+  </div>
+</BrowserRouter>
 </div>
 
   )
