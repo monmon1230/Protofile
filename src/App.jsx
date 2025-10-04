01736420ef854ee8f041af78b11da0463f39a425
mina magdy <mina@magdy.io>
2025-10-04 14:56:18 +0300

    Add build files

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 import React, { useState } from "react";
-import './App.css';
-import HomePage from './Components/HomePage/Pages/HomePage';
-import AboutPage from './Components/HomePage/Pages/AboutPage';
-import Contactme from './Components/HomePage/Pages/Contactme';
-import Projects from './Components/HomePage/Pages/Projects';
-import NavBar from './Components/HomePage/NavBar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Dimentions } from './constants/dimensions';
+import NavBar from "./Components/HomePage/NavBar";
+import HomePage from "./Components/HomePage/Pages/HomePage";
+import AboutPage from "./Components/HomePage/Pages/AboutPage";
+import Contactme from "./Components/HomePage/Pages/Contactme";
+import Projects from "./Components/HomePage/Pages/Projects";
+import { Dimentions } from "./constants/dimensions";
+import './App.css';
 
 function App() {
   const [activeLink, setActiveLink] = useState("home");
 
-  const handleClick = (link) => {
-    setActiveLink(link);
-  };
+  const handleClick = (link) => setActiveLink(link);
 
   return (
-      <div className='w-screen h-screen p-2 bg-gradient-to-br from-blue-900 via-black to-red-900'>
+    <div className="w-screen h-screen p-2 bg-gradient-to-br from-blue-900 via-black to-red-900">
       <BrowserRouter basename="/Protofile">
         <NavBar activeLink={activeLink} handleClick={handleClick} />
         <div className={`mt-[${Dimentions.navHeight}]`}>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<Contactme />} />
-            <Route path="/projects" element={<Projects />} />
+            <Route path="/Protofile/about" element={<AboutPage />} />
+            <Route path="/Protofile/contact" element={<Contactme />} />
+            <Route path="/Protofile/projects" element={<Projects />} />
           </Routes>
         </div>
       </BrowserRouter>
